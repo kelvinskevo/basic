60c27bdf058eb86fff8a144d24b211be27069baa
kelvins <kelvinchemiati@gmail.com>
2024-07-15 19:10:44 +0300

    password_recovery_update

--- resources/views/admin/body/header.blade.php
+++ resources/views/admin/body/header.blade.php
@@ -22,8 +22,8 @@
                 </a>
             </div>
 
-            <button type="button" class="btn btn-sm px-3 font-size-24 header-item waves-effect" id="vertical-menu-btn">
-                <i class="ri-menu-2-line align-middle"></i>
+            <button type="button" class="px-3 btn btn-sm font-size-24 header-item waves-effect" id="vertical-menu-btn">
+                <i class="align-middle ri-menu-2-line"></i>
             </button>
 
             <!-- App Search-->
@@ -63,18 +63,26 @@
                 </button>
                 <div class="dropdown-menu dropdown-menu-end">
                     <!-- item-->
-                    <a class="dropdown-item" href="#"><i class="ri-user-line align-middle me-1"></i>
+                    <a class="dropdown-item" href="#"><i class="align-middle ri-user-line me-1"></i>
                         Profile</a>
-                    <a class="dropdown-item" href="#"><i class="ri-wallet-2-line align-middle me-1"></i> My
+                    <a class="dropdown-item" href="#"><i class="align-middle ri-wallet-2-line me-1"></i> My
                         Wallet</a>
                     <a class="dropdown-item d-block" href="#"><span
-                            class="badge bg-success float-end mt-1">11</span><i
-                            class="ri-settings-2-line align-middle me-1"></i> Settings</a>
-                    <a class="dropdown-item" href="#"><i class="ri-lock-unlock-line align-middle me-1"></i>
+                            class="mt-1 badge bg-success float-end">11</span><i
+                            class="align-middle ri-settings-2-line me-1"></i> Settings</a>
+                    <a class="dropdown-item" href="#"><i class="align-middle ri-lock-unlock-line me-1"></i>
                         Lock screen</a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item text-danger" href="#"><i
-                            class="ri-shut-down-line align-middle me-1 text-danger"></i> Logout</a>
+                    <!-- Hidden logout form -->
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+
+                    <!-- Logout link -->
+                    <a class="dropdown-item text-danger" href="#"
+                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        <i class="align-middle ri-shut-down-line me-1 text-danger"></i> Logout
+                    </a>
                 </div>
             </div>
 
--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,89 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Recover Password | Upcube - Admin & Dashboard Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesdesign" name="author" />
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
+    <link href="{{ asset('backend/assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet"
+        type="text/css" />
+    <link href="{{ asset('backend/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('backend/assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
+</head>
+
+<body class="auth-body-bg">
+    <div class="bg-overlay"></div>
+    <div class="wrapper-page">
+        <div class="p-0 container-fluid">
+            <div class="card">
+                <div class="card-body">
+
+                    <div class="mt-4 text-center">
+                        <div class="mb-3">
+                            <a href="{{ route('home') }}" class="auth-logo">
+                                <img src="{{ asset('backend/assets/images/logo-dark.png') }}" height="30"
+                                    class="mx-auto logo-dark" alt="">
+                                <img src="{{ asset('backend/assets/images/logo-light.png') }}" height="30"
+                                    class="mx-auto logo-light" alt="">
+                            </a>
+                        </div>
+                    </div>
+
+                    <h4 class="text-center text-muted font-size-18"><b>Reset Password</b></h4>
+
+                    <div class="p-3">
+                        <div class="mb-4 text-sm text-gray-600">
+                            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+                        </div>
+
+                        <!-- Session Status -->
+                        <x-auth-session-status class="mb-4" :status="session('status')" />
+
+                        <form class="mt-3 form-horizontal" method="POST" action="{{ route('password.email') }}">
+                            @csrf
+
+                            <div class="alert alert-info alert-dismissible fade show" role="alert">
+                                Enter Your <strong>E-mail</strong> and instructions will be sent to you!
+                                <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                    aria-label="Close"></button>
+                            </div>
+
+                            <div class="mb-3 form-group">
+                                <div class="col-xs-12">
+                                    <x-input-label for="email" :value="__('Email')" />
+                                    <x-text-input id="email" class="block w-full mt-1 form-control" type="email"
+                                        name="email" :value="old('email')" required autofocus />
+                                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                </div>
+                            </div>
+
+                            <div class="pb-2 mt-3 text-center form-group row">
+                                <div class="col-12">
+                                    <button class="btn btn-info w-100 waves-effect waves-light" type="submit">Send
+                                        Email</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <!-- end cardbody -->
+            </div>
+            <!-- end card -->
+        </div>
+        <!-- end container -->
     </div>
+    <!-- end -->
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+    <script src="{{ asset('backend/assets/libs/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/metismenu/metisMenu.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/simplebar/simplebar.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/node-waves/waves.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/app.js') }}"></script>
 
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
+</body>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+</html>
